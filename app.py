26ee22be838acb77d73380bb5e82bb7ccd32acf2
Vitaly Tsurikov <tsurikow.v@gmail.com>
2024-12-22 20:02:38 +0300

    Updated project files

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import time
-from multiprocess import Pool
+from multiprocessing import Pool
 import asyncio
 import plotly.graph_objects as go
 from utils.city import city_data_processing
