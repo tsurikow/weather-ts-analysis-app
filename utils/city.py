9af1e3dcfed8295ee2e6453bbac95bec7949ed4a
Vitaly Tsurikov <tsurikow.v@gmail.com>
2024-12-22 23:42:54 +0300

    Updated project files

--- utils/city.py
+++ utils/city.py
@@ -1,5 +1,4 @@
 import statsmodels.api as sm
-from statsmodels.tsa.statespace import exponential_smoothing
 import pandas as pd
 import numpy as np
 
@@ -7,7 +6,7 @@ def trend_sarimax(data, city, steps=12):
     # SARIMAX для прогноза долгосрочного тренда
     city_ts = data[data['city'] == city]['temperature']
     city_ts_M = pd.DataFrame(city_ts.resample('ME').mean())
-    city_M_sarimax = sm.tsa.SARIMAX(endog=city_ts_M, order=[1,1,0], seasonal_order=[1,1,1,12], simple_differencing=True).fit(maxiter=200, method='lbfgs', disp=False)
+    city_M_sarimax = sm.tsa.SARIMAX(endog=city_ts_M, order=[1,1,0], seasonal_order=[1,1,1,12], simple_differencing=True).fit(maxiter=200, method='bfgs', disp=False)
     forecast = city_M_sarimax.forecast(steps=steps)
     city_ts_final = pd.DataFrame(city_ts_M).merge(
         pd.DataFrame(forecast).rename(columns = {'predicted_mean':'forecast'}),
@@ -26,32 +25,7 @@ def trend_sarimax(data, city, steps=12):
         trend = 'Probably decreasing mean temp'
     return trend
 
-def trend_ses(data, city, steps=12):
-    city_ts = data[data['city'] == city]['temperature']
-    city_ts_M = pd.DataFrame(city_ts.resample('ME').mean())
-    ES = exponential_smoothing.ExponentialSmoothing
-    config = {"trend": True}
-    stlf = sm.tsa.STLForecast(city_ts_M, ES, model_kwargs=config)
-    res = stlf.fit(maxiter=200, method='bfgs', disp=False)
-    forecast = res.forecast(steps)
-    city_ts_final = pd.DataFrame(city_ts_M).merge(
-        pd.DataFrame(forecast).rename(columns={'predicted_mean': 'forecast'}),
-        how='outer', left_index=True, right_index=True)
-    future_mean = city_ts_final['forecast'].iloc[-steps:].mean()
-    past_mean = city_ts_final['temperature'].iloc[-steps * 2:-steps].mean()
-    future_last_mean = city_ts_final['forecast'].iloc[-12:].mean()
-    trend = 'Unknown'
-    if past_mean > future_mean > future_last_mean:
-        trend = 'Decreasing mean temp'
-    elif past_mean < future_mean < future_last_mean:
-        trend = 'Increasing mean temp'
-    elif future_mean < future_last_mean:
-        trend = 'Probably increasing mean temp'
-    elif future_mean > future_last_mean:
-        trend = 'Probably decreasing mean temp'
-    return trend
-
-def city_data_processing(data, city, window=30, steps=12):
+def city_data_processing(data, city, window=30, steps=36):
     city_data = data[data['city'] == city]
     city_data['smoothed'] = np.convolve(city_data['temperature'], np.ones(window)/window, 'same')
     city_data['season_mean'] = city_data.groupby(['season'])['temperature'].transform('mean')
@@ -64,6 +38,5 @@ def city_data_processing(data, city, window=30, steps=12):
         np.nan,
         city_data['temperature'])
     trend = trend_sarimax(data, city, steps)
-    #trend = trend_ses(data, city, steps)
 
     return city_data, trend, city
